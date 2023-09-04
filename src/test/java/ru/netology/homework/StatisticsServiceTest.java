941337b55ad936aacbfb555b06fc3ed57c092bb0
Olga <kornilovaolga1488@gmail.com>
2023-09-04 12:14:18 +0300

    change commit

---
+++ src/test/java/ru/netology/homework/StatisticsServiceTest.java
@@ -0,0 +1,33 @@
+package ru.netology.homework;
+
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.Assertions;
+
+public class StatisticsServiceTest {
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxTwo() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {2, 6, 3, 7, 4, 0, 10, 8, 2, 7, 9};
+        long expected = 10;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+}
